2657b8c19682294a69b906ffe396dd5e09d5eaaa
LIXUANXI\李宣西、 <wangguotong@foxmail.com>
2020-10-19 21:54:06 +0800

    Init Project

---
+++ Project/ds_twr_init/platform/lcd_oled.c
@@ -0,0 +1,441 @@
+#include "lcd_oled.h"
+#include "stdlib.h"
+#include "lcd_oledfont.h"
+#include "deca_sleep.h"
+//OLEDµÄÏÔ´æ
+//´æ·Å¸ñÊ½ÈçÏÂ.
+//[0]0 1 2 3 ... 127
+//[1]0 1 2 3 ... 127
+//[2]0 1 2 3 ... 127
+//[3]0 1 2 3 ... 127
+//[4]0 1 2 3 ... 127
+//[5]0 1 2 3 ... 127
+//[6]0 1 2 3 ... 127
+//[7]0 1 2 3 ... 127
+
+
+
+///I2C related
+/*******************************************************************************
+* Function Name  : I2C_delay
+* Description    : ÑÓ³ÙÊ±¼ä
+* Input          : None
+* Output         : None
+* Return         : None
+* Attention      : None
+*******************************************************************************/
+static void I2C_delay(uint16_t cnt)
+{
+    while(cnt--);
+}
+
+#define IIC_TIMEOUT 100
+int IIc_flag = 1 ;//´æÔÚ1£¬ ²»´æÔÚ0
+/*******************************************************************************
+* Function Name  : I2C_AcknowledgePolling
+* Description    : µÈ´ý»ñÈ¡I2C×ÜÏß¿ØÖÆÈ¨ ÅÐ¶ÏÃ¦×´Ì¬
+* Input          : - I2Cx:I2C¼Ä´æÆ÷»ùÖ·
+*                  - I2C_Addr:´ÓÆ÷¼þµØÖ·
+* Output         : None
+* Return         : None
+* Attention      : None
+*******************************************************************************/
+static void I2C_AcknowledgePolling(I2C_TypeDef *I2Cx,uint8_t I2C_Addr)
+{
+    vu16 SR1_Tmp;
+    uint16_t timeout = 10000;
+    timeout = IIC_TIMEOUT;
+    do
+    {
+        I2C_GenerateSTART(I2Cx, ENABLE); /*ÆðÊ¼Î»*/
+        /*¶ÁSR1*/
+        SR1_Tmp = I2C_ReadRegister(I2Cx, I2C_Register_SR1);
+        /*Æ÷¼þµØÖ·(Ð´)*/
+        I2C_Send7bitAddress(I2Cx, I2C_Addr, I2C_Direction_Transmitter);
+
+    }
+    while(!(I2C_ReadRegister(I2Cx, I2C_Register_SR1) & 0x0002)&&(timeout--));
+    I2C_ClearFlag(I2Cx, I2C_FLAG_AF);
+    I2C_GenerateSTOP(I2Cx, ENABLE);  /*Í£Ö¹Î»*/
+}
+
+
+/*******************************************************************************
+* Function Name  : I2C_WriteOneByte
+* Description    : Í¨¹ýÖ¸¶¨I2C½Ó¿ÚÐ´ÈëÒ»¸ö×Ö½ÚÊý¾Ý
+* Input          : - I2Cx:I2C¼Ä´æÆ÷»ùÖ·
+*                  - I2C_Addr:´ÓÆ÷¼þµØÖ·
+*                  - addr:Ô¤Ð´Èë×Ö½ÚµØÖ·
+*                  - value:Ð´ÈëÊý¾Ý
+* Output         : None
+* Return         : ³É¹¦·µ»Ø0
+* Attention      : None
+*******************************************************************************/
+uint8_t I2C_WriteOneByte(I2C_TypeDef *I2Cx,uint8_t I2C_Addr,uint8_t addr,uint8_t value)
+{
+    uint16_t timeout = 0;
+    /* ÆðÊ¼Î» */
+    I2C_GenerateSTART(I2Cx, ENABLE);
+    timeout = IIC_TIMEOUT;
+    while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_MODE_SELECT) &&(timeout--));
+    I2C_Send7bitAddress(I2Cx, I2C_Addr, I2C_Direction_Transmitter);
+
+    if(timeout == 0)
+        IIc_flag = 0;
+
+    timeout = IIC_TIMEOUT;
+    while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED)&&(timeout--));
+    /*·¢ËÍµØÖ·*/
+    I2C_SendData(I2Cx, addr);
+    timeout = IIC_TIMEOUT;
+    while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_TRANSMITTED)&&(timeout--));
+    /* Ð´Ò»¸ö×Ö½Ú*/
+    I2C_SendData(I2Cx, value);
+    timeout = IIC_TIMEOUT;
+    while(!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_TRANSMITTED)&&(timeout--));
+    /* Í£Ö¹Î»*/
+    I2C_GenerateSTOP(I2Cx, ENABLE);
+    /*stop bit flag*/
+    //while(I2C_GetFlagStatus(I2Cx, I2C_FLAG_STOPF));
+
+    I2C_AcknowledgePolling(I2Cx,I2C_Addr);
+    I2C_delay(80);
+    return 0;
+}
+
+
+uint8_t I2C_ReadOneByte(I2C_TypeDef* I2Cx, unsigned char AddressDevice, unsigned char AddressByte)
+{
+    uint8_t ReceiveData = 0;
+
+    while(I2C_GetFlagStatus(I2Cx, I2C_FLAG_BUSY));
+    I2C_GenerateSTART(I2Cx, ENABLE);
+    while( !I2C_GetFlagStatus(I2Cx, I2C_FLAG_SB) ); // wait Generate Start
+
+    I2C_Send7bitAddress(I2Cx, AddressDevice, I2C_Direction_Transmitter);
+    while( !I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED) ); // wait send Address Device
+
+    I2C_SendData(I2Cx, AddressByte);
+    while( !I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_TRANSMITTED) ); // wait send Address Byte
+
+    I2C_GenerateSTART(I2Cx, ENABLE);
+    while( !I2C_GetFlagStatus(I2Cx, I2C_FLAG_SB) ); // wait Generate Start
+
+    I2C_Send7bitAddress(I2Cx, AddressDevice, I2C_Direction_Receiver);
+    while( !I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED) ); // wait Send Address Device As Receiver
+
+    while( !I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_RECEIVED) ); // wait Receive a Byte
+    ReceiveData = I2C_ReceiveData(I2Cx);
+
+    I2C_NACKPositionConfig(I2Cx, I2C_NACKPosition_Current); // send not acknowledge
+    I2C_AcknowledgeConfig(I2Cx, DISABLE);// disable acknowledge
+
+    I2C_GenerateSTOP(I2Cx, ENABLE);
+    while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_STOPF)); // wait Generate Stop Condition
+
+    I2C_AcknowledgeConfig(I2Cx, DISABLE);// disable acknowledge
+
+    return ReceiveData;
+};
+
+
+/*******************************************************************************
+* Function Name  : I2C_Write
+* Description    : Í¨¹ýÖ¸¶¨I2C½Ó¿ÚÐ´Èë¶à¸ö×Ö½ÚÊý¾Ý
+* Input          : - I2Cx:I2C¼Ä´æÆ÷»ùÖ·
+*                  - I2C_Addr:´ÓÆ÷¼þµØÖ·
+*                  - addr:Ô¤Ð´Èë×Ö½ÚµØÖ·
+*                  - *buf:Ô¤Ð´ÈëÊý¾Ý´æ´¢Î»ÖÃ
+*                  - num:Ð´Èë×Ö½ÚÊý
+* Output         : None
+* Return         : ³É¹¦·µ»Ø0
+* Attention      : None
+*******************************************************************************/
+uint8_t I2C_Write(I2C_TypeDef *I2Cx,uint8_t I2C_Addr,uint8_t addr,uint8_t *buf,uint16_t num)
+{
+    uint8_t err=0;
+
+    while(num--)
+    {
+        if(I2C_WriteOneByte(I2Cx, I2C_Addr,addr++,*buf++))
+        {
+            err++;
+        }
+    }
+    if(err)
+        return 1;
+    else
+        return 0;
+}
+
+
+//STM32F103C8T6 IIC OLED
+#ifdef STM32F10X_MD
+void OLED_WR_Byte(uint8_t dat,uint8_t cmd)
+{
+    if(IIc_flag == 1)
+    {
+        if(cmd)
+        {
+            I2C_WriteOneByte(I2C1,0x78,0x40,dat);
+        }
+        else
+        {
+            I2C_WriteOneByte(I2C1,0x78,0x00,dat);
+        }
+    }
+}
+
+#else
+//STM32F103RCT6 SPI OLED
+///I2C related end
+
+
+#if OLED_MODE==1
+//ÏòSSD1106Ð´ÈëÒ»¸ö×Ö½Ú¡£
+//dat:ÒªÐ´ÈëµÄÊý¾Ý/ÃüÁî
+//cmd:Êý¾Ý/ÃüÁî±êÖ¾ 0,±íÊ¾ÃüÁî;1,±íÊ¾Êý¾Ý;
+void OLED_WR_Byte(uint8_t dat,uint8_t cmd)
+{
+    DATAOUT(dat);
+    if(cmd)
+        OLED_DC_Set();
+    else
+        OLED_DC_Clr();
+    OLED_CS_Clr();
+    OLED_WR_Clr();
+    OLED_WR_Set();
+    OLED_CS_Set();
+    OLED_DC_Set();
+}
+#else
+//ÏòSSD1106Ð´ÈëÒ»¸ö×Ö½Ú¡£
+//dat:ÒªÐ´ÈëµÄÊý¾Ý/ÃüÁî
+//cmd:Êý¾Ý/ÃüÁî±êÖ¾ 0,±íÊ¾ÃüÁî;1,±íÊ¾Êý¾Ý;
+void OLED_WR_Byte(uint8_t dat,uint8_t cmd)
+{
+    uint8_t i;
+    if(cmd)
+        OLED_DC_Set();
+    else
+        OLED_DC_Clr();
+    OLED_CS_Clr();
+    for(i=0; i<8; i++)
+    {
+        OLED_SCLK_Clr();
+        if(dat&0x80)
+            OLED_SDIN_Set();
+        else
+            OLED_SDIN_Clr();
+        OLED_SCLK_Set();
+        dat<<=1;
+    }
+    OLED_CS_Set();
+    OLED_DC_Set();
+}
+#endif
+#endif
+void OLED_Set_Pos(unsigned char x, unsigned char y)
+{
+    OLED_WR_Byte(0xb0+y,OLED_CMD);
+    OLED_WR_Byte(((x&0xf0)>>4)|0x10,OLED_CMD);
+    OLED_WR_Byte((x&0x0f)|0x01,OLED_CMD);
+}
+//¿ªÆôOLEDÏÔÊ¾
+void OLED_Display_On(void)
+{
+    OLED_WR_Byte(0X8D,OLED_CMD);  //SET DCDCÃüÁî
+    OLED_WR_Byte(0X14,OLED_CMD);  //DCDC ON
+    OLED_WR_Byte(0XAF,OLED_CMD);  //DISPLAY ON
+}
+//¹Ø±ÕOLEDÏÔÊ¾
+void OLED_Display_Off(void)
+{
+    OLED_WR_Byte(0X8D,OLED_CMD);  //SET DCDCÃüÁî
+    OLED_WR_Byte(0X10,OLED_CMD);  //DCDC OFF
+    OLED_WR_Byte(0XAE,OLED_CMD);  //DISPLAY OFF
+}
+//ÇåÆÁº¯Êý,ÇåÍêÆÁ,Õû¸öÆÁÄ»ÊÇºÚÉ«µÄ!ºÍÃ»µãÁÁÒ»Ñù!!!
+void OLED_Clear(void)
+{
+    uint8_t i,n;
+    for(i=0; i<8; i++)
+    {
+        OLED_WR_Byte (0xb0+i,OLED_CMD);    //ÉèÖÃÒ³µØÖ·£¨0~7£©
+        OLED_WR_Byte (0x00,OLED_CMD);      //ÉèÖÃÏÔÊ¾Î»ÖÃ¡ªÁÐµÍµØÖ·
+        OLED_WR_Byte (0x10,OLED_CMD);      //ÉèÖÃÏÔÊ¾Î»ÖÃ¡ªÁÐ¸ßµØÖ·
+        for(n=0; n<128; n++)OLED_WR_Byte(0,OLED_DATA);
+    } //¸üÐÂÏÔÊ¾
+}
+
+
+//ÔÚÖ¸¶¨Î»ÖÃÏÔÊ¾Ò»¸ö×Ö·û,°üÀ¨²¿·Ö×Ö·û
+//x:0~127
+//y:0~63
+//mode:0,·´°×ÏÔÊ¾;1,Õý³£ÏÔÊ¾
+//size:Ñ¡Ôñ×ÖÌå 16/12
+void OLED_ShowChar(uint8_t x,uint8_t y,uint8_t chr)
+{
+    unsigned char c=0,i=0;
+    c=chr-' ';//µÃµ½Æ«ÒÆºóµÄÖµ
+    if(x>Max_Column-1)
+    {
+        x=0;
+        y=y+2;
+    }
+    if(SIZE ==16)
+    {
+        OLED_Set_Pos(x,y);
+        for(i=0; i<8; i++)
+            OLED_WR_Byte(F8X16[c*16+i],OLED_DATA);
+        OLED_Set_Pos(x,y+1);
+        for(i=0; i<8; i++)
+            OLED_WR_Byte(F8X16[c*16+i+8],OLED_DATA);
+    }
+    else
+    {
+        OLED_Set_Pos(x,y+1);
+        for(i=0; i<6; i++)
+            OLED_WR_Byte(F6x8[c][i],OLED_DATA);
+
+    }
+}
+//m^nº¯Êý
+uint32_t oled_pow(uint8_t m,uint8_t n)
+{
+    uint32_t result=1;
+    while(n--)result*=m;
+    return result;
+}
+//ÏÔÊ¾2¸öÊý×Ö
+//x,y :Æðµã×ø±ê
+//len :Êý×ÖµÄÎ»Êý
+//size:×ÖÌå´óÐ¡
+//mode:Ä£Ê½ 0,Ìî³äÄ£Ê½;1,µþ¼ÓÄ£Ê½
+//num:ÊýÖµ(0~4294967295);
+void OLED_ShowNum(uint8_t x,uint8_t y,uint32_t num,uint8_t len,uint8_t size)
+{
+    uint8_t t,temp;
+    uint8_t enshow=0;
+    for(t=0; t<len; t++)
+    {
+        temp=(num/oled_pow(10,len-t-1))%10;
+        if(enshow==0&&t<(len-1))
+        {
+            if(temp==0)
+            {
+                OLED_ShowChar(x+(size/2)*t,y,' ');
+                continue;
+            }
+            else enshow=1;
+
+        }
+        OLED_ShowChar(x+(size/2)*t,y,temp+'0');
+    }
+}
+//ÏÔÊ¾Ò»¸ö×Ö·ûºÅ´®
+void OLED_ShowString(uint8_t x,uint8_t y,uint8_t *chr)
+{
+    unsigned char j=0;
+    while (chr[j]!='\0')
+    {
+        OLED_ShowChar(x,y,chr[j]);
+        x+=8;
+        if(x>120)
+        {
+            x=0;
+            y+=2;
+        }
+        j++;
+    }
+}
+//ÏÔÊ¾ºº×Ö
+void OLED_ShowCHinese(uint8_t x,uint8_t y,uint8_t no)
+{
+    uint8_t t,adder=0;
+    OLED_Set_Pos(x,y);
+    for(t=0; t<16; t++)
+    {
+        OLED_WR_Byte(Hzk[2*no][t],OLED_DATA);
+        adder+=1;
+    }
+    OLED_Set_Pos(x,y+1);
+    for(t=0; t<16; t++)
+    {
+        OLED_WR_Byte(Hzk[2*no+1][t],OLED_DATA);
+        adder+=1;
+    }
+}
+/***********¹¦ÄÜÃèÊö£ºÏÔÊ¾ÏÔÊ¾BMPÍ¼Æ¬128¡Á64ÆðÊ¼µã×ø±ê(x,y),xµÄ·¶Î§0¡«127£¬yÎªÒ³µÄ·¶Î§0¡«7*****************/
+void OLED_DrawBMP(unsigned char x0, unsigned char y0,unsigned char x1, unsigned char y1,unsigned char BMP[])
+{
+    unsigned int j=0;
+    unsigned char x,y;
+
+    if(y1%8==0) y=y1/8;
+    else y=y1/8+1;
+    for(y=y0; y<y1; y++)
+    {
+        OLED_Set_Pos(x0,y);
+        for(x=x0; x<x1; x++)
+        {
+            OLED_WR_Byte(BMP[j++],OLED_DATA);
+        }
+    }
+}
+
+
+//³õÊ¼»¯SSD1306
+void OLED_Init(void)
+{
+#ifdef STM32F10X_HD
+//STM32F103RC SPI OLED
+    GPIO_InitTypeDef  GPIO_InitStructure;
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);    //Ê¹ÄÜA¶Ë¿ÚÊ±ÖÓ
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_15;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;         //ÍÆÍìÊä³ö
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;//ËÙ¶È50MHz
+    GPIO_Init(GPIOB, &GPIO_InitStructure);    //³õÊ¼»¯GPIOD3,6
+    GPIO_SetBits(GPIOB,GPIO_Pin_10|GPIO_Pin_11|GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_15);
+
+    OLED_RST_Set();
+    deca_sleep(100);
+    OLED_RST_Clr();
+    deca_sleep(200);
+    OLED_RST_Set();
+#endif
+
+    OLED_WR_Byte(0xAE,OLED_CMD);//--turn off oled panel
+    OLED_WR_Byte(0x00,OLED_CMD);//---set low column address
+    OLED_WR_Byte(0x10,OLED_CMD);//---set high column address
+    OLED_WR_Byte(0x40,OLED_CMD);//--set start line address  Set Mapping RAM Display Start Line (0x00~0x3F)
+    OLED_WR_Byte(0x81,OLED_CMD);//--set contrast control register
+    OLED_WR_Byte(0xCF,OLED_CMD); // Set SEG Output Current Brightness
+    OLED_WR_Byte(0xA1,OLED_CMD);//--Set SEG/Column Mapping     0xa0×óÓÒ·´ÖÃ 0xa1Õý³£
+    OLED_WR_Byte(0xC8,OLED_CMD);//Set COM/Row Scan Direction   0xc0ÉÏÏÂ·´ÖÃ 0xc8Õý³£
+    OLED_WR_Byte(0xA6,OLED_CMD);//--set normal display
+    OLED_WR_Byte(0xA8,OLED_CMD);//--set multiplex ratio(1 to 64)
+    OLED_WR_Byte(0x3f,OLED_CMD);//--1/64 duty
+    OLED_WR_Byte(0xD3,OLED_CMD);//-set display offset   Shift Mapping RAM Counter (0x00~0x3F)
+    OLED_WR_Byte(0x00,OLED_CMD);//-not offset
+    OLED_WR_Byte(0xd5,OLED_CMD);//--set display clock divide ratio/oscillator frequency
+    OLED_WR_Byte(0x80,OLED_CMD);//--set divide ratio, Set Clock as 100 Frames/Sec
+    OLED_WR_Byte(0xD9,OLED_CMD);//--set pre-charge period
+    OLED_WR_Byte(0xF1,OLED_CMD);//Set Pre-Charge as 15 Clocks & Discharge as 1 Clock
+    OLED_WR_Byte(0xDA,OLED_CMD);//--set com pins hardware configuration
+    OLED_WR_Byte(0x12,OLED_CMD);
+    OLED_WR_Byte(0xDB,OLED_CMD);//--set vcomh
+    OLED_WR_Byte(0x40,OLED_CMD);//Set VCOM Deselect Level
+    OLED_WR_Byte(0x20,OLED_CMD);//-Set Page Addressing Mode (0x00/0x01/0x02)
+    OLED_WR_Byte(0x02,OLED_CMD);//
+    OLED_WR_Byte(0x8D,OLED_CMD);//--set Charge Pump enable/disable
+    OLED_WR_Byte(0x14,OLED_CMD);//--set(0x10) disable
+    OLED_WR_Byte(0xA4,OLED_CMD);// Disable Entire Display On (0xa4/0xa5)
+    OLED_WR_Byte(0xA6,OLED_CMD);// Disable Inverse Display On (0xa6/a7)
+    OLED_WR_Byte(0xAF,OLED_CMD);//--turn on oled panel
+
+    OLED_WR_Byte(0xAF,OLED_CMD); /*display ON*/
+    OLED_Clear();
+    OLED_Set_Pos(0,0);
+}
